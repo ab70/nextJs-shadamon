6eca3d38736c44638e92b0fd5610e358603a3058
ab70 <nurulabrar2369@gmail.com>
2023-01-31 16:01:22 +0600

    changes

--- app/page.jsx
+++ app/page.jsx
@@ -1,91 +1,62 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+"use client";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import { useState } from "react";
+import styles from "./page.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+
+const signUpInitialState = {
+  name: "",
+  emailphone: "",
+  password: "",
+};
 
 export default function Home() {
+  const [signUp, setSignUp] = useState();
+
+  //function
+  const onInputChange = (e) => {
+    setSignUp({ ...signUp, [e.target.name]: e.target.value });
+    
+  };
+  const finalObj = (x)=>{
+    console.log(x);
+    let finalData ={}
+    if(x==true){
+      finalData = {
+        name: signUp.name,
+        email: signUp.emailphone,
+        password: signUp.password
+      }
+      
+    }
+    else{
+      finalData = {
+        name: signUp.name,
+        phone: signUp.emailphone,
+        password: signUp.password
+      }
+    }
+    console.log(finalData);
+
+  }
+  //submit form 
+  const submitForm = ()=>{
+    console.log(isNaN(signUp.emailphone));
+    let checkValue = isNaN(signUp.emailphone)
+    checkValue ? finalObj(checkValue) : finalObj(checkValue)
+  }
+
   return (
     <>
-
-<button type="button" class="px-6
-      py-2.5
-      bg-blue-600
-      text-white
-      font-medium
-      text-xs
-      leading-tight
-      uppercase
-      rounded
-      shadow-md
-      hover:bg-blue-700 hover:shadow-lg
-      focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-      active:bg-blue-800 active:shadow-lg
-      transition
-      duration-150
-      ease-in-out" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
-</button>
-
-
-<div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto"
-  id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog relative w-auto pointer-events-none">
-    <div
-      class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
-      <div
-        class="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
-        <h5 class="text-xl font-medium leading-normal text-gray-800" id="exampleModalLabel">Modal title</h5>
-        <button type="button"
-          class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
-          data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body relative p-4">
-        Modal body text goes here.
-      </div>
-      <div
-        class="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
-        <button type="button" class="px-6
-          py-2.5
-          bg-purple-600
-          text-white
-          font-medium
-          text-xs
-          leading-tight
-          uppercase
-          rounded
-          shadow-md
-          hover:bg-purple-700 hover:shadow-lg
-          focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0
-          active:bg-purple-800 active:shadow-lg
-          transition
-          duration-150
-          ease-in-out" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="px-6
-      py-2.5
-      bg-blue-600
-      text-white
-      font-medium
-      text-xs
-      leading-tight
-      uppercase
-      rounded
-      shadow-md
-      hover:bg-blue-700 hover:shadow-lg
-      focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-      active:bg-blue-800 active:shadow-lg
-      transition
-      duration-150
-      ease-in-out
-      ml-1">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-<div class="flex justify-center">
-  <div>
-    <div class="form-floating mb-3 xl:w-96">
-      <input type="email" class="form-control
+      <div className="flex justify-center">
+        <div>
+          <form method="post">
+            <div className="form-floating mb-3 xl:w-96">
+              <input
+                type="text"
+                className="form-control
       block
       w-full
       px-3
@@ -99,11 +70,20 @@ export default function Home() {
       transition
       ease-in-out
       m-0
-      focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="floatingInput" placeholder="name@example.com"/>
-      <label for="floatingInput" class="text-gray-700">Email address</label>
-    </div>
-    <div class="form-floating mb-3 xl:w-96">
-      <input type="password" class="form-control
+      focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                name="name"
+                onChange={(e) => onInputChange(e)}
+                id="floatingInput"
+                placeholder="name@example.com"
+              />
+              <label for="floatingInput" className="text-gray-700">
+                Name
+              </label>
+            </div>
+            <div className="form-floating mb-3 xl:w-96">
+              <input
+                type="text"
+                className="form-control
       block
       w-full
       px-3
@@ -117,11 +97,47 @@ export default function Home() {
       transition
       ease-in-out
       m-0
-      focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="floatingPassword" placeholder="Password"/>
-      <label for="floatingPassword" class="text-gray-700">Password</label>
-    </div>
-  </div>
-</div>
+      focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                name="emailphone"
+                onChange={(e) => onInputChange(e)}
+                id="floatingInput"
+                placeholder="name@example.com"
+              />
+              <label for="floatingInput" className="text-gray-700">
+                Email or phone
+              </label>
+            </div>
+            <div className="form-floating mb-3 xl:w-96">
+              <input
+                type="password"
+                className="form-control
+      block
+      w-full
+      px-3
+      py-1.5
+      text-base
+      font-normal
+      text-gray-700
+      bg-white bg-clip-padding
+      border border-solid border-gray-300
+      rounded
+      transition
+      ease-in-out
+      m-0
+      focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                name="password"
+                onChange={(e) => onInputChange(e)}
+                id="floatingPassword"
+                placeholder="Password"
+              />
+              <label for="floatingPassword" className="text-gray-700">
+                Password
+              </label>
+            </div>
+            <button type="button" onClick={submitForm} class="inline-block px-6 py-2.5 bg-green-500 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out">Success</button>
+          </form>
+        </div>
+      </div>
     </>
-  )
+  );
 }
